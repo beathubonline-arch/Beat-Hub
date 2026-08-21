d7485b3cf85880eee2330ee1bb0989368001aac5
beathubonline-arch <beathubonline@gmail.com>
2026-08-21 11:26:07 +0300

    Create 0001_baseline.py

---
+++ alembic/versions/0001_baseline.py
@@ -0,0 +1,22 @@
+"""BeatHub migration baseline.
+
+Revision ID: beathub_baseline_001
+Revises:
+Create Date: 2026-08-21
+"""
+
+from alembic import op
+
+
+revision = "beathub_baseline_001"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
