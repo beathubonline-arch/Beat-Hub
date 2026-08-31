417b6579a27d705e12d44dc882b83a49c8000136
beathubonline-arch <beathubonline@gmail.com>
2026-09-01 02:21:07 +0300

    Register Paystack settlement model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,7 @@ from app.models.profile import Profile
 from app.models.music import Track, Album, AlbumTrack, SalesModel, ProductCurrency
 from app.models.order import Order, OrderStatus, License
 from app.models.payment import PaymentTransaction, PaymentStatus
+from app.models.paystack_settlement import PaystackSettlement
 from app.models.ledger import (
     CreatorLedgerEntry,
     PlatformLedgerEntry,
@@ -14,7 +15,7 @@ from app.models.ledger import (
 
 __all__ = [
     "User", "UserRole", "Profile", "Track", "Album", "AlbumTrack", "SalesModel", "ProductCurrency",
-    "Order", "OrderStatus", "License", "PaymentTransaction", "PaymentStatus",
+    "Order", "OrderStatus", "License", "PaymentTransaction", "PaymentStatus", "PaystackSettlement",
     "CreatorLedgerEntry", "PlatformLedgerEntry", "WithdrawalRequest", "WithdrawalStatus",
     "AdminWithdrawal", "AdminWithdrawalStatus",
 ]
