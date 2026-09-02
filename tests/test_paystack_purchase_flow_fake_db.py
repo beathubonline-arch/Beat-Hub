c3d19a24665cc9d634932f802f20acd601142c71
beathubonline-arch <beathubonline@gmail.com>
2026-09-02 06:57:14 +0300

    Remove temporary Paystack fixture test

--- tests/test_paystack_purchase_flow_fake_db.py
+++
@@ -1,8 +0,0 @@
-from tests.test_paystack_purchase_flow import FakeDB, FakeQuery
-
-
-def test_fake_db_execute_compatibility():
-    db = FakeDB()
-    assert hasattr(db, "query")
-    assert hasattr(db, "get")
-    assert hasattr(FakeQuery(None), "one_or_none")
