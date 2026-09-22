bcc190ed7212af13287f0f0e6ea4cc670819e04d
beathubonline-arch <beathubonline@gmail.com>
2026-09-22 05:57:55 +0300

    Register release campaign model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -17,10 +17,11 @@ from app.models.ledger import (
 from app.models.growth import GrowthProspect, GrowthTouch, GrowthExperiment
 from app.models.growth_runs import GrowthAgentRun
 from app.models.session import SessionService, SessionBooking
+from app.models.release_campaign import ReleaseCampaign
 
 __all__ = [
     "User", "UserRole", "Profile", "Track", "Album", "AlbumTrack", "SalesModel", "ProductCurrency",
     "Order", "OrderStatus", "License", "PaymentTransaction", "PaymentStatus", "PaystackSettlement", "PaystackSettlementTransaction",
     "Notification", "PushSubscription", "CreatorLedgerEntry", "PlatformLedgerEntry", "WithdrawalRequest", "WithdrawalStatus",
-    "AdminWithdrawal", "AdminWithdrawalStatus", "GrowthProspect", "GrowthTouch", "GrowthExperiment", "GrowthAgentRun", "SessionService", "SessionBooking",
+    "AdminWithdrawal", "AdminWithdrawalStatus", "GrowthProspect", "GrowthTouch", "GrowthExperiment", "GrowthAgentRun", "SessionService", "SessionBooking", "ReleaseCampaign",
 ]
